55038313147de854a99c84bdcffaaab9a371a1d0
Pranamya Kulal <spacedust26@gmail.com>
2024-06-29 10:20:48 +0530

    login regiser responsiveness

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -11,39 +11,56 @@ const LoginForm = () => {
   const [error, setError] = useState("")
   const router = useRouter()
 
-  const handleSubmit = async(e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
+    try {
       const res = await signIn("credentials", {
         email,
         password,
         redirect: false,
       })
-      if(res.error){
+      if (res.error) {
         setError("Invalid Credentials")
         return;
       }
       router.replace("dashboard")
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
 
   return (
-    <div className='container w-full h-[100vh] flex items-center justify-center text-white'>
-        <form onSubmit={handleSubmit} action='' className='form bg-[#321E1E] p-[50px] rounded-lg w-[500px] h-[600px] flex flex-col justify-center items-center gap-5'>
-            <Image src="/gif/EventziaLogo.gif" alt="logo" width={120} height={120} />
-            <h1 className='text-white font-bold text-2xl'>Login</h1>
-            {error && (<div className='text-sm text-red-600'>{error}</div>)}
+    <div className='container w-full h-[100vh] flex items-center justify-center text-white p-4 md:p-0'>
+      <form
+        onSubmit={handleSubmit}
+        action=''
+        className='form bg-[#321E1E] p-6 md:p-[50px] rounded-lg w-full max-w-[500px] h-auto md:h-[600px] flex flex-col justify-center items-center gap-5'
+      >
+        <Image src="/gif/EventziaLogo.gif" alt="logo" width={120} height={120} />
+        <h1 className='text-white font-bold text-2xl'>Login</h1>
+        {error && (<div className='text-sm text-red-600'>{error}</div>)}
 
-            <input type='text' placeholder='Enter Email' className='p-[20px] border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full' onChange={(e) => setEmail(e.target.value) } />
+        <input
+          type='text'
+          placeholder='Enter Email'
+          className='p-4 border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full'
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-            <input type='password' placeholder='Enter Password' className='p-[20px] border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full' onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type='password'
+          placeholder='Enter Password'
+          className='p-4 border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full'
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-            <button className='p-[20px] bg-[#d4af37] rounded-lg text-[#321E1E] border-none cursor-pointer w-full'>Login</button>
-            <Link className="text-sm" href={"/register"}>Don't have an account? <span className='underline'>Register</span></Link>
-        </form>
+        <button className='p-4 bg-[#d4af37] rounded-lg text-[#321E1E] border-none cursor-pointer w-full'>Login</button>
+        <Link className="text-sm" href={"/register"}>
+          Don't have an account? <span className='underline'>Register</span>
+        </Link>
+      </form>
     </div>
+
   )
 }
 
--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
-const RegisterForm = () =>{
+const RegisterForm = () => {
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -29,8 +29,8 @@ const RegisterForm = () =>{
           email
         }),
       })
-      const {user} = await resUserExists.json();
-      if(user){
+      const { user } = await resUserExists.json();
+      if (user) {
         setError("User already exists")
         return;
       }
@@ -44,11 +44,11 @@ const RegisterForm = () =>{
           username, email, password,
         }),
       })
-      if (res.ok){
+      if (res.ok) {
         const form = e.target;
         form.reset();
         router.push("/login")
-      }else{
+      } else {
         console.log("User registration failed")
       }
     } catch (error) {
@@ -57,22 +57,50 @@ const RegisterForm = () =>{
   }
 
   return (
-    <div className='container w-full h-[100vh] flex items-center justify-center text-white'>
-      <form onSubmit={handleSubmit} className='form bg-[#321E1E] p-[50px] rounded-lg w-[500px] h-[650px] flex flex-col justify-center items-center gap-5'>
+    <div className='container w-full h-[100vh] flex items-center justify-center text-white p-4 md:p-0'>
+      <form
+        onSubmit={handleSubmit}
+        className='form bg-[#321E1E] p-6 md:p-[50px] rounded-lg w-full max-w-[500px] h-auto md:h-[650px] flex flex-col justify-center items-center gap-5'
+      >
         <Image src="/gif/EventziaLogo.gif" alt="logo" width={120} height={120} />
         <h1 className='text-white font-bold text-2xl'>Register</h1>
         {error && (<div className='text-sm text-red-600'>{error}</div>)}
 
-        <input type='text' placeholder='Enter username' className='p-[20px] border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full' onChange={(e) => setUsername(e.target.value)} value={username} />
+        <input
+          type='text'
+          placeholder='Enter username'
+          className='p-4 border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full'
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
 
-        <input type='text' placeholder='Enter Email' className='p-[20px] border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full' onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input
+          type='text'
+          placeholder='Enter Email'
+          className='p-4 border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
 
-        <input type='password' placeholder='Enter Password' className='p-[20px] border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full' onChange={(e) => setPassword(e.target.value)} value={password} />
+        <input
+          type='password'
+          placeholder='Enter Password'
+          className='p-4 border border-[#503C3C] rounded-lg bg-[#321E1E] text-white w-full'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
 
-        <button className='p-[20px] bg-[#d4af37] rounded-lg text-[#321E1E] border-none cursor-pointer w-full'>Register</button>
-        <Link className="text-sm" href={"/login"}>Already have an account? <span className='underline'>Login</span></Link>
+        <button
+          className='p-4 bg-[#d4af37] rounded-lg text-[#321E1E] border-none cursor-pointer w-full'
+        >
+          Register
+        </button>
+        <Link className="text-sm" href={"/login"}>
+          Already have an account? <span className='underline'>Login</span>
+        </Link>
       </form>
     </div>
+
   )
 }
 
